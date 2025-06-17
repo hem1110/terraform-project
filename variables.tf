620df0d135b15fcc1deff5768bcf46674fecbf33
Hemanth Kumar <calc.sh>
2025-06-17 17:06:47 -0400

    Defined variable types explicitly

--- variables.tf
+++ variables.tf
@@ -5,18 +5,24 @@ variable "rg_name" {
 variable "location" {
   type        = string
 }
-variable "subscription_id" {
-  description = "Azure subscription ID"
-}
 
 variable "client_id" {
-  description = " Azure Client ID"
+  description = "Azure Client ID"
+  type        = string
 }
 
 variable "client_secret" {
   description = "Azure Client Secret"
+  type        = string
   sensitive   = true
 }
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
 variable "tenant_id" {
   description = "Azure Tenant ID"
+  type        = string
 }
